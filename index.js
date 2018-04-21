4a95d71b144e55b58ee752e86e3c8be8090b5675
Dieter Van Hoof <dieter.vanhoof@c4j.be>
2018-04-21 13:41:15 +0200

    Changed recipient

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8081;
 
 app.get('/', (req, res) => {
-	res.json({ message: 'Hello Dieter!' });
+	res.json({ message: 'Hello World!' });
 });
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT + '.'));
